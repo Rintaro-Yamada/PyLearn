661e4f25a86b68a54e4427f9f61b68db9c5e8e70
Rintaro Yamada <RintaroRobert14sei@gmail.com>
2020-09-24 11:56:39 +0900

    modifies

--- BayesianOptimization/bo.py
+++ BayesianOptimization/bo.py
@@ -4,7 +4,7 @@ import random
 import math
 from nptyping import NDArray
 import matplotlib.pyplot as plt
-#unresolved import 'numpy'Python(unresolved-import)
+from sklearn.kernel_approximation import RBFSampler
 
 def func(x: NDArray[float]) -> NDArray[float]: #テスト用のforrester関数
     # 最大化したいので符号を反転
@@ -20,7 +20,12 @@ class RBFKernel():
         temp2 = np.c_[np.sum(X2**2, axis=1)]
         norm = temp1 + temp2.T - 2*np.dot(X1,X2.T)
         return self.variance * np.exp(-norm / (2*self.length_scale**2))
-        
+
+def RFM(x:NDArray[float],dim:int) -> NDArray[float]:
+
+    phi=math.sqrt(2/dim)*math.cos(np.dot(omega,x)+c)
+
+
 def plot(mu: NDArray[float], var: NDArray[float],X:NDArray[float],y:NDArray[float],X_train:NDArray[float],y_train:NDArray[float]):
     plt.rcParams["font.size"] = 13
     plt.subplot(1, 1, 1)
@@ -30,7 +35,7 @@ def plot(mu: NDArray[float], var: NDArray[float],X:NDArray[float],y:NDArray[floa
     plt.fill_between(X.ravel(), (mu + 2 * np.sqrt(var)).ravel(), (mu - 2 * np.sqrt(var)).ravel(), alpha=0.3, color="blue")
     plt.plot(X_train.ravel(), y_train.ravel(), "ro", label="observed")
     plt.legend(loc="lower left",prop={'size': 8})
-    plt.show()
+    plt.savefig("GP.pdf")
     plt.close()
 
 def experiment(seed: int, initial_num: int, max_iter: int):
@@ -66,7 +71,8 @@ def experiment(seed: int, initial_num: int, max_iter: int):
     #初期データの結果のプロット
     plot(pred_mu, pred_var_diag, X, y, X_train, y_train)
 
-    #獲得関数MESの作成
+    #RFMからy*をサンプリング
+
     
 
 
@@ -81,12 +87,13 @@ if __name__ == "__main__":
     main()
 
 
-''' ガウス過程回帰モデル　の　作り方
+''' ガウス過程回帰モデル（MES）の作り方
 
 1. 初期(訓練)データを用意 p81
 2. 関数を用意
 3. カーネル何使おうか？決める(今回はRBF)
 4. カーネル行列の作成
+5. RFMの作成
 5. 活性化関数の作成(MES)
 5. 新しい入力点x'の出力y'を求める
 
